84a036cb30e95b215d9b0b57827af762a4ade715
Eduardo Braga <chocolate.blindado123@gmail.com>
2023-01-20 19:56:28 -0300

    Entendendo os tipos primitivos

---
+++ codes_002/index.js
@@ -0,0 +1,18 @@
+// Tipos Primitivos
+//Number
+var x = 10;
+console.log(typeof x, x, x.toPrecision(2));
+var y = 5.890;
+console.log(y, y.toFixed(3));
+//String
+var firstName = 'Carlos';
+var lastName = 'Eduardo';
+var fullname;
+fullname = firstName + " " + lastName;
+console.log(fullname);
+console.log(fullname.toUpperCase());
+//Boolean
+var a = false;
+console.log(a, typeof a);
+a = true;
+console.log(a);
---
+++ codes_002/index.ts
@@ -0,0 +1,30 @@
+// Tipos Primitivos
+
+//Number
+let x: number = 10
+
+console.log(typeof x, x, x.toPrecision(2))
+
+let y: number = 5.890
+
+console.log(y, y.toFixed(3))
+
+//String
+const firstName: string = 'Carlos'
+const lastName: string = 'Eduardo'
+
+let fullname: string
+
+fullname = firstName + " " + lastName
+
+console.log(fullname)
+console.log(fullname.toUpperCase())
+
+//Boolean
+let a: boolean = false
+
+console.log(a, typeof a)
+
+a = true
+
+console.log(a)
