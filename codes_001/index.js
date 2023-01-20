1a29a178e28a41bf21236f59911c43fc9f34dcbd
Eduardo Braga <chocolate.blindado123@gmail.com>
2023-01-20 17:39:55 -0300

    Aprendi como compilar e executar TS

---
+++ codes_001/index.js
@@ -0,0 +1,9 @@
+var fisrtName = 'Eduardo';
+var secondName = 7;
+var age = true;
+var logMsg = function (msg) {
+    console.log(msg);
+};
+logMsg(fisrtName);
+// logMsg(secondName)
+// logMsg(age)
---
+++ codes_001/index.ts
@@ -0,0 +1,11 @@
+const fisrtName = 'Eduardo'
+const secondName = 7
+const age = true
+
+const logMsg = (msg: string) => {
+  console.log(msg)
+}
+
+logMsg(fisrtName)
+// logMsg(secondName)
+// logMsg(age)
