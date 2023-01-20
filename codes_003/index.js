41d991ebaa377364771a8a68bf93624f49aaeb87
Eduardo Braga <chocolate.blindado123@gmail.com>
2023-01-20 20:28:31 -0300

    arquivo de config e type annotation/inference

---
+++ codes_003/index.js
@@ -0,0 +1,6 @@
+"use strict";
+//Type annotation e type inference
+let x = 'X';
+let y = 'Y';
+console.log(y, x);
+console.log('hi');
---
+++ codes_003/index.ts
@@ -0,0 +1,7 @@
+//Type annotation e type inference
+let x: string = 'X'
+let y = 'Y'
+
+console.log(y, x)
+
+console.log('hi')
