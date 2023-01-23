89ea31cfabbc949922a4148461191a17fc9d1586
Eduardo Braga <chocolate.blindado123@gmail.com>
2023-01-23 10:09:39 -0300

    Entendendo props opcionais e sua validação

--- codes_006/index.js
+++ codes_006/index.js
@@ -9,3 +9,22 @@ const newUser = (user) => ({
 const eduardo = newUser(Object.assign(Object.assign({}, user), { age: 19 }));
 const jose = newUser({ name: 'josé', surname: 'Antonio', age: 87 });
 console.log(jose, eduardo);
+//Props opcionais
+const showNames = (name1, name2, name3) => {
+    console.log(`Nome 1: ${name1}`);
+    console.log(`Nome 2: ${name2}`);
+    if (name3)
+        console.log(`Nome 3: ${name3}`);
+};
+showNames('edu', 'dudu', 'cadu');
+showNames('edu', 'dudu');
+//Validando props opcinais
+const advancedGreeting = (fisrtName, lastName) => {
+    const fullName = `${fisrtName} ${lastName}`;
+    if (lastName !== undefined) {
+        return `Olá, ${fullName}, tudo bom?`;
+    }
+    return `Olá, ${fisrtName}, tudo bom?`;
+};
+console.log(advancedGreeting('Eduardo', 'Braga'));
+console.log(advancedGreeting('Vivian'));
--- codes_006/index.ts
+++ codes_006/index.ts
@@ -11,3 +11,31 @@ const eduardo = newUser({...user, age: 19})
 const jose = newUser({name: 'josé', surname: 'Antonio', age: 87})
 
 console.log(jose, eduardo)
+
+
+//Props opcionais
+const showNames = (name1: string, name2: string, name3?: string) => {
+  console.log(`Nome 1: ${name1}`)
+  console.log(`Nome 2: ${name2}`)
+
+  if(name3) 
+    console.log(`Nome 3: ${name3}`)
+}
+
+showNames('edu', 'dudu', 'cadu')
+showNames('edu', 'dudu')
+
+
+//Validando props opcinais
+const advancedGreeting = (fisrtName: string, lastName?: string):string => {
+  const fullName = `${fisrtName} ${lastName}`
+
+  if(lastName !== undefined){
+    return `Olá, ${fullName}, tudo bom?`
+  }
+
+  return `Olá, ${fisrtName}, tudo bom?`
+}
+
+console.log(advancedGreeting('Eduardo', 'Braga'))
+console.log(advancedGreeting('Vivian'))
