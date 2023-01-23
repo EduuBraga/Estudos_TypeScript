46205535639c099f9df72263298e775b3637f644
Eduardo Braga <chocolate.blindado123@gmail.com>
2023-01-23 11:11:04 -0300

    Entendendo a interface

--- codes_008/index.js
+++ codes_008/index.js
@@ -6,3 +6,12 @@ showId(34);
 showId('55');
 const obj = { name: 'eduardo', age: '19', id: 77 };
 console.log(obj);
+const showUser = (user) => {
+    console.log(`O usuário ${user.name} tem ${user.age} anos.`);
+};
+const userEduardo = {
+    name: 'Eduardo',
+    age: 19,
+    id: 776
+};
+showUser(userEduardo);
--- codes_008/index.ts
+++ codes_008/index.ts
@@ -12,3 +12,24 @@ const obj: { name: string, age: strOrNum, id: strOrNum } =
   { name: 'eduardo', age: '19', id: 77 }
 
 console.log(obj)
+
+
+//interface
+interface User {
+  name: string,
+  age: number,
+  id: string | number
+}
+
+const showUser = (user: User) => {
+  console.log(`O usuário ${user.name} tem ${user.age} anos.`)
+}
+
+const userEduardo: User = {
+  name: 'Eduardo',
+  age: 19,
+  id: 776
+}
+
+showUser(userEduardo)
+
