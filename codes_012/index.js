90844f6accc04ff1a4b4cbe1a0587a2f780b4233
Eduardo Braga <chocolate.blindado123@gmail.com>
2023-01-24 16:27:44 -0300

    Vendo um pouco mais de interface

---
+++ codes_012/index.js
@@ -0,0 +1,29 @@
+"use strict";
+const showProductDetails = ({ name, price, isAvailable }) => {
+    console.log(`O produto ${name} está custando R$${price}`);
+    isAvailable
+        ? console.log(`${name} está disponível`)
+        : console.log(`${name} não está disponível`);
+};
+const shirt = {
+    name: 'camisa',
+    price: 55.50,
+    isAvailable: true
+};
+const pants = {
+    name: 'calça',
+    price: 99,
+    isAvailable: false
+};
+showProductDetails(shirt);
+showProductDetails(pants);
+const showUserEmail = (user) => {
+    console.log(`Este é o seu email: ${user.email}`);
+    user.role
+        ? console.log(`Você tem como função: ${user.role}`)
+        : console.log(`Você não tem função`);
+};
+const u1 = { email: 'dudu@email.com', password: '123', role: 'admin' };
+const u2 = { email: 'vivi@email.com', password: '456' };
+showUserEmail(u1);
+showUserEmail(u2);
---
+++ codes_012/index.ts
@@ -0,0 +1,51 @@
+// - Interface
+interface Product {
+  name: string,
+  price: number,
+  isAvailable: boolean
+}
+
+const showProductDetails = ({ name, price, isAvailable }: Product) => {
+  console.log(`O produto ${name} está custando R$${price}`)
+
+  isAvailable
+    ? console.log(`${name} está disponível`)
+    : console.log(`${name} não está disponível`)
+}
+
+const shirt: Product = {
+  name: 'camisa',
+  price: 55.50,
+  isAvailable: true
+}
+
+const pants: Product = {
+  name: 'calça',
+  price: 99,
+  isAvailable: false
+}
+
+showProductDetails(shirt)
+showProductDetails(pants)
+
+
+// - interface com props opcionais
+interface User {
+  email: string,
+  password: string,
+  role?: string
+}
+
+const showUserEmail = (user: User) => {
+  console.log(`Este é o seu email: ${user.email}`)
+
+  user.role
+    ? console.log(`Você tem como função: ${user.role}`)
+    : console.log(`Você não tem função`)
+}
+
+const u1: User = {email: 'dudu@email.com', password: '123', role: 'admin'}
+const u2: User = {email: 'vivi@email.com', password: '456'}
+
+showUserEmail(u1)
+showUserEmail(u2)
